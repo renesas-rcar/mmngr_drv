5722c951520317ecd669033f4ea4bfe6be5cb2f7
Khiem Nguyen <khiem.nguyen.xt@rvc.renesas.com>
2016-07-05 08:23:23 +0700

    mmngr_drv: mmngr: Support phys2virt for Lossy area
    When IPMMU is enabled and IP master access Lossy area, the virt2phys
    function is needed.
    This patch adds virt2phys function support for Lossy area.
    
    Signed-off-by: Khiem Nguyen <khiem.nguyen.xt@rvc.renesas.com>

--- mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
+++ mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
@@ -803,11 +803,20 @@ static phys_addr_t pmb_virt2phys(unsigned int ipmmu_virt_addr)
 {
 	phys_addr_t cpu_phys_addr;
 
-	if ((ipmmu_virt_addr >= CMA_1ST_VIRT_BASE_ADDR) &
-	    (ipmmu_virt_addr < (CMA_1ST_VIRT_BASE_ADDR
-				+ mm_common_reserve_size))) {
+	/*
+	 * For Common CMA and CMA for Lossy,
+	 *   assign physical address equal to virtual address.
+	 * For CMA for MMP,
+	 *   do address conversion.
+	 */
+	if (((ipmmu_virt_addr >= CMA_LOSSY_VIRT_BASE_ADDR) &&
+	     (ipmmu_virt_addr < (CMA_LOSSY_VIRT_BASE_ADDR
+				 + mm_lossybuf_size))) ||
+	    ((ipmmu_virt_addr >= CMA_1ST_VIRT_BASE_ADDR) &&
+	     (ipmmu_virt_addr < (CMA_1ST_VIRT_BASE_ADDR
+				 + mm_common_reserve_size)))) {
 		cpu_phys_addr = ipmmu_virt_addr;
-	} else if ((ipmmu_virt_addr >= CMA_2ND_VIRT_BASE_ADDR) &
+	} else if ((ipmmu_virt_addr >= CMA_2ND_VIRT_BASE_ADDR) &&
 		(ipmmu_virt_addr < (CMA_2ND_VIRT_BASE_ADDR
 					+ mm_kernel_reserve_addr))) {
 		cpu_phys_addr = ((ipmmu_virt_addr - CMA_2ND_VIRT_BASE_ADDR)
