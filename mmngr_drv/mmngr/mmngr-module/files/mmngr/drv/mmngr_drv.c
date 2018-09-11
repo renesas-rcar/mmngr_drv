renesas-rcar/mmngr_drv

commit 611f026ca9624ed7d8fa78beffc91a2f8b888d40

mmngr_drv: mmngr: Change the way of parsing reserved memory regions in DT
Should not use dt path for parsing memory node by index.
Instead, loop through phandle index to find the node
and retrieve its info.

Signed-off-by: Hung Manh Le <hung.le.xh@rvc.renesas.com>
Signed-off-by: Hien Duy Dang <hien.dang.eb@renesas.com>
Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
@@ -1058,51 +1058,63 @@ static int validate_memory_map(void)
 	return ret;
 }
 
-static int _parse_reserved_mem_dt(const char *dt_path,
+static int _parse_reserved_mem_dt(struct device_node *np,
+				  const char *phandle_name,
+				  const char *match,
 				  u64 *addr, u64 *size)
 {
 	const __be32 *regaddr_p;
 	struct device_node *node;
+	int index;
+	int prop_size = 0;
+	int ret = 0;
+	*addr = 0;
+	*size = 0;
 
-	node = of_find_node_by_path(dt_path);
-	if (!node)
-		return -1;
-
-	regaddr_p = of_get_address(node, 0, size, NULL);
-	if (!regaddr_p) {
-		of_node_put(node);
-		return -1;
+	of_get_property(np, phandle_name, &prop_size);
+	for (index = 0; index < prop_size; index++) {
+		node = of_parse_phandle(np, phandle_name, index);
+		if (node) {
+			if (strstr(match, node->name)) {
+				regaddr_p = of_get_address(node, 0, size, NULL);
+				break;
+			}
+		}
 	}
 
-	*addr = of_translate_address(node, regaddr_p);
+	if (regaddr_p && (index < prop_size))
+		*addr = of_translate_address(node, regaddr_p);
+	else
+		ret = -1;
 
 	of_node_put(node);
-
-	return 0;
+	return ret;
 }
 
-static int parse_reserved_mem_dt(void)
+static int parse_reserved_mem_dt(struct device_node *np)
 {
 	int ret = 0;
 
-	ret = _parse_reserved_mem_dt(
-			"/reserved-memory/linux,multimedia",
-			&mm_kernel_reserve_addr, &mm_kernel_reserve_size);
+	/* Parse reserved memory for multimedia */
+	ret = _parse_reserved_mem_dt(np, "memory-region",
+				     "linux,multimedia",
+				     &mm_kernel_reserve_addr,
+				     &mm_kernel_reserve_size);
 	if (ret) {
 		pr_warn("Failed to parse MMP reserved area" \
 			 "(linux,multimedia) from DT\n");
 		return ret;
 	}
-
-	ret = _parse_reserved_mem_dt(
-			"/reserved-memory/linux,lossy_decompress",
-			&mm_lossybuf_addr, &mm_lossybuf_size);
+	/* Parse reserved memory for lossy compression feature */
+	ret = _parse_reserved_mem_dt(np, "memory-region",
+				     "linux,lossy_decompress",
+				     &mm_lossybuf_addr,
+				     &mm_lossybuf_size);
 	if (ret) {
 		pr_warn("Failed to parse Lossy reserved area" \
 			"(linux,lossy_decompress) from DT\n");
 		ret = 0; /* Let MMNGR support other features */
 	}
-
 	return ret;
 }
 
@@ -1560,9 +1572,10 @@ static int mm_probe(struct platform_device *pdev)
 	phys_addr_t		phy_addr;
 	void			*pkernel_virt_addr;
 	struct device		*dev = &pdev->dev;
+	struct device_node	*np = dev->of_node;
 	unsigned long		mm_omxbuf_size;
 
-	ret = parse_reserved_mem_dt();
+	ret = parse_reserved_mem_dt(np);
 	if (ret) {
 		pr_err("%s MMD ERROR\n", __func__);
 		return -1;

mmngr_drv/mmngr/mmngr-module/files/mmngr/include/mmngr_private.h
@@ -188,9 +188,11 @@ static int mm_ioc_share(int __user *in, struct MM_PARAM *out);
 static void mmngr_dev_set_cma_area(struct device *dev, struct cma *cma);
 static int init_lossy_info(void);
 static int find_lossy_entry(unsigned int flag, int *entry);
-static int _parse_reserved_mem_dt(const char *dt_path,
+static int _parse_reserved_mem_dt(struct device_node *np,
+				  const char *phandle_name,
+				  const char *match,
 				  u64 *addr, u64 *size);
-static int parse_reserved_mem_dt(void);
+static int parse_reserved_mem_dt(struct device_node *np);
 static int open(struct inode *inode, struct file *file);
 static int close(struct inode *inode, struct file *file);
 static long ioctl(struct file *file, unsigned int cmd, unsigned long arg);

commit b5875bf80a63b31094d1a508e06a8579ee7a255c

mmngr_drv: mmngr: Fix uninitialized variables
Fix below smatch (v0.5.0-4481-g31420c3c71d3) errors:
  _parse_reserved_mem_dt() error: uninitialized symbol 'regaddr_p'.
  _parse_reserved_mem_dt() error: uninitialized symbol 'node'.

Fixes: 611f026ca962 ("mmngr_drv: mmngr: Change the way of parsing reserved memory regions in DT")
Signed-off-by: Eugeniu Rosca <erosca@de.adit-jv.com>
Signed-off-by: Andrew Gabbasov <andrew_gabbasov@mentor.com>
Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
@@ -1081,8 +1081,8 @@ static int _parse_reserved_mem_dt(struct device_node *np,
 				  const char *match,
 				  u64 *addr, u64 *size)
 {
-	const __be32 *regaddr_p;
-	struct device_node *node;
+	const __be32 *regaddr_p = NULL;
+	struct device_node *node = NULL;
 	int index;
 	int prop_size = 0;
 	int ret = 0;