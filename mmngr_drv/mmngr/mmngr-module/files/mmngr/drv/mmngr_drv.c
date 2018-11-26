renesas-rcar/mmngr_drv

commit df328808f6c4a8fe13e9a11c69f6d946ed875a0d

mmngr_drv: mmngr: Disable IPMMU TLB cache function in r8a77990
As a work-around for IPMMU H/W restriction No.100 on r8a77990 Ver1.0,
disable IPMMU TLB cache function of IPMMU caches that belong to
A3xx power domain:
  - IPMMU-VP0
  - IPMMU-VC0

Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
@@ -1428,7 +1428,9 @@ static int ipmmu_probe(struct platform_device *pdev)
 	else
 		rcar_gen3_ipmmu = data->ipmmu_data;
 
-	if (soc_device_match(r8a7795) || soc_device_match(r8a77965))
+	if (soc_device_match(r8a7795) ||
+	    soc_device_match(r8a77965) ||
+	    soc_device_match(r8a77990))
 		is_mmu_tlb_disabled = true;
 	else
 		is_mmu_tlb_disabled = false;

commit e83338c78b0cbebdd71bbba6e52e05d95a8cfb01

mmngr_drv: mmngr: Only disable TLB function of IPMMU-VC0 cache on E3 Ver.1.x
As a work-around for IPMMU H/W restriction No.100 on r8a77990 Ver.1.x,
disable IPMMU TLB cache function of IPMMU caches that
belongs to A3xx power domain:
	- IPMMU-VC0

IPMMU-VP0 is now belong to Always-on power domain and will be
handled by IPL.

Fixes: df328808f6c4 ('mmngr_drv: mmngr: Disable IPMMU TLB cache function in r8a77990')

Signed-off-by: An Nguyen <an.nguyen.zc@rvc.renesas.com>
Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

mmngr_drv/mmngr/mmngr-module/files/mmngr/drv/mmngr_drv.c
@@ -515,6 +515,11 @@ static struct rcar_ipmmu *r8a77990_ipmmu[] = {
 	NULL, /* End of list */
 };
 
+static struct rcar_ipmmu *r8a77990_disable_mmu_tlb[] = {
+	&r8a77990_ipmmuvc0,
+	NULL, /* End of list */
+};
+
 #endif /* IPMMU_MMU_SUPPORT */
 
 static int mm_ioc_alloc(struct device *mm_dev,
@@ -1782,8 +1787,13 @@ static int ipmmu_mmu_initialize(void)
 	__handle_registers(&ipmmumm, SET_TRANSLATION_TABLE);
 	__handle_registers(&ipmmumm, ENABLE_MMU_MM);
 
-	if (is_mmu_tlb_disabled)
-		handle_registers(rcar_gen3_ipmmu, DISABLE_MMU_TLB);
+	if (is_mmu_tlb_disabled) {
+		if (soc_device_match(r8a77990es1))
+			handle_registers(r8a77990_disable_mmu_tlb,
+					 DISABLE_MMU_TLB);
+		else
+			handle_registers(rcar_gen3_ipmmu, DISABLE_MMU_TLB);
+	}
 	handle_registers(rcar_gen3_ipmmu, ENABLE_MMU);
 	handle_registers(rcar_gen3_ipmmu, ENABLE_UTLB);
 	__handle_registers(&ipmmumm, CLEAR_MMU_STATUS_REGS);